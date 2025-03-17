1df4b35b25a746c3f854f4b2bedc9fa3b7f9553c
John Fedoruk <johnny@johnfedoruk.ca>
2025-03-17 15:58:03 -0500

    Added warning about NextJS client scope polluting main bundle

--- src/ui/components/molecules/Logo/Logo.tsx
+++ src/ui/components/molecules/Logo/Logo.tsx
@@ -7,6 +7,15 @@ import React from "react";
  * 
  * Be sure to add "use client" to the top
  * line of your pages that include Image component.
+ * 
+ * WARNING!
+ * Including dependencies that require browser
+ * context to render will pollute the main bundle
+ * so it will require that "use client" be used
+ * for all pages importing package components,
+ * or package components that run in a server
+ * context will need to be imported from a directory
+ * level that does not contain client context.
  */
 // import Image from "next/image";
 
